aec3a99245a38799dc595d9e29c5085ee52bb1e0
DilanBernal28 <dilanbernal01@gmail.com>
2025-03-26 23:46:01 -0500

    feat: more separated json, photo animation
    Se separo en los json de traduccion por secciones, en home va lo de hero section, y en header lo de header, adeparte se agrego la animacion de la photo con un bucle for de scss

---
+++ src/app/features/home/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-section',
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+
+}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { NgbScrollSpyModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSectionComponent],
+  standalone: true,
+  imports: [HeroSectionComponent, NgbScrollSpyModule, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
